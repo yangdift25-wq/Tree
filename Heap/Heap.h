dac745f8689d5c1f63f6bd403296c27c7b73777e
xinke yang <yangdift25@gmail.com>
2026-03-18 20:15:34 +0800

    Add Heap data structure header file

---
+++ Heap/Heap.h
@@ -0,0 +1,24 @@
+#pragma once
+#include<stdio.h>
+#include<assert.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+typedef int HPDataType;
+
+//¶Ñ
+typedef struct Heap
+{
+	HPDataType* a;
+	int size;
+	int capacity;
+}HP;
+
+void HPInit(HP* php);
+void HPDestroy(HP* php);
+void HPPush(HP* php,HPDataType x);
+void HPPop(HP* php);
+void AdjustDown(HPDataType* a, int n, int parent);
+void AdjustUp(HPDataType* a, int child);
+HPDataType HPTop(HP* php);
+bool HPEmpty(HP* php);
